fd17f03c0053037ad30f6da0ea79a06426deabc3
Mike Shelby <mikegshelby@gmail.com>
2020-10-19 20:45:22 -0500

    create additional JS code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var quizBtn = document.querySelector(".btn-start");
+var mainContentEl = document.querySelector(".main-content");
 
 // Object constructor function for all quiz questions/answers
 function quizQuestion(questionText, answer1, answer2, answer3, answer4, correctAnswer) {
@@ -21,24 +21,6 @@ var quizQuestion4 = new quizQuestion("String values must be enclosed within ____
 
 var quizQuestion5 = new quizQuestion("A very useful tool used during development and debugging for printing content to the debugger is:", "JavaScript", "terminal/bash", "for loops", "console.log", "console.log");
 
-// All quiz question/answer object variables saved in an array
-var allQuestions = [quizQuestion1, quizQuestion2, quizQuestion3, quizQuestion4, quizQuestion5]
-
-// Function will loop through each quiz question with four possible answers
-var quizLoop = function() {
-    for (i = 0; i < allQuestions.length; i++) {
-
-        var userResponse = window.prompt(allQuestions[i].questionText + "   1. " + allQuestions[i].answer1 + "   2. " + allQuestions[i].answer2 + "   3. " + allQuestions[i].answer3 + "   4. " + allQuestions[i].answer4);
-
-        if (userResponse === allQuestions[i].correctAnswer) {
-                window.alert("Correct!");
-        }
-        else {
-            window.alert("False!");
-            //Deduct time on the timer
-        }
-    }
-}
 
 var createStartEl = function() {
     // create start section
@@ -52,75 +34,108 @@ var createStartEl = function() {
     var startQuizBtn = document.createElement("button");
     startQuizBtn.className = "btn-start";
     startQuizBtn.innerHTML = "Start Quiz";
-
     startSectionEl.appendChild(startQuizBtn);
+
+    startButton = document.querySelector("#btn-start");
 
     // get main content element
     mainContentEl = document.querySelector(".main-content");
     // add start section to main content element
     mainContentEl.appendChild(startSectionEl);
-
 }
 
 createStartEl();
 
 
-// This function creates quiz elements, including quiz question, answer options, and answer feedback. This will need the following parameters from whichever object is being iterated in the quiz function: quiz
-var createQuizEl = function() {
-    // create quiz section
-    var quizSectionEl = document.createElement("section");
-    quizSectionEl.className = "quiz-section";
 
-    // create question heading element
-    var questionTitleEl = document.createElement("h3");
-    questionTitleEl.className = "question-title";
-    questionTitleEl.innerHTML = "QUESTION TEXT";
-    quizSectionEl.appendChild(questionTitleEl);
 
-    // create ordered list element
-    var answerListEl  = document.createElement("ol");
-    answerListEl.className = "answer-list";
+// This function creates HTML quiz elements, including section, h3, ol, and buttons
+var createQuizEl = function(event) {
+            var startSectionEl = document.querySelector(".start-section");
+            startSectionEl.remove();
 
-    // create answer button 1 and append to list
-    var answerBtn1 = document.createElement("button");
-    answerBtn1.className = "btn";
-    answerBtn1.innerHTML = "<li>" + "ANSWER 1" + "</li>";
-    answerListEl.appendChild(answerBtn1);
+            // create quiz section
+            var quizSectionEl = document.createElement("section");
+            quizSectionEl.className = "quiz-section";
 
-    // create answer button 2
-    var answerBtn2 = document.createElement("button");
-    answerBtn2.className = "btn";
-    answerBtn2.innerHTML = "<li>" + "ANSWER 2" + "</li>";
-    answerListEl.appendChild(answerBtn2);
+            // create question heading element
+            var questionTitleEl = document.createElement("h3");
+            questionTitleEl.className = "question-title";
+            quizSectionEl.appendChild(questionTitleEl);
 
-    // create answer button 3
-    var answerBtn3 = document.createElement("button");
-    answerBtn3.className = "btn";
-    answerBtn3.innerHTML = "<li>" + "ANSWER 3" + "</li>";
-    answerListEl.appendChild(answerBtn3);
+            // create ordered list element
+            var answerListEl  = document.createElement("ol");
+            answerListEl.className = "answer-list";
 
-    // create answer button 4
-    var answerBtn4 = document.createElement("button");
-    answerBtn4.className = "btn";
-    answerBtn4.innerHTML = "<li>" + "ANSWER 4" + "</li>";
-    answerListEl.appendChild(answerBtn4);
+            // create answer button 1
+            var answerBtn1 = document.createElement("button");
+            answerBtn1.className = "btn";
+            answerBtn1.setAttribute("id", "1");
+            answerListEl.appendChild(answerBtn1);
 
-    // append title and list to new section element
-    quizSectionEl.appendChild(answerListEl);
+            // create answer button 2
+            var answerBtn2 = document.createElement("button");
+            answerBtn2.className = "btn";
+            answerBtn2.setAttribute("id", "2");
+            answerListEl.appendChild(answerBtn2);
 
+            // create answer button 3
+            var answerBtn3 = document.createElement("button");
+            answerBtn3.className = "btn";
+            answerBtn3.setAttribute("id", "3");
+            answerListEl.appendChild(answerBtn3);
+
+            // create answer button 4
+            var answerBtn4 = document.createElement("button");
+            answerBtn4.className = "btn";
+            answerBtn4.setAttribute("id", "4");
+            answerListEl.appendChild(answerBtn4);
+
+            // append title and list to new section element
+            quizSectionEl.appendChild(answerListEl);
+
+            // get main content element
+            mainContentEl = document.querySelector(".main-content");
+            // add quiz section to main content element
+            mainContentEl.appendChild(quizSectionEl);
+
+            addQuestionInfo(questionTitleEl, answerBtn1, answerBtn2, answerBtn3, answerBtn4, 0);
+
+
+}
+
+
+
+// responseValidator = function(event) {
+//     var buttonSelection = event.target.getAttribute("id");
+//     console.log(buttonSelection);
+// }
+
+
+
+var feedbackEl = function() {
     // create answer feedback footer
     var answerFeedbackEl = document.createElement("footer");
     answerFeedbackEl.className = "answer-feedback";
     answerFeedbackEl.innerHTML = "ANSWER FEEDBACK";
     quizSectionEl.appendChild(answerFeedbackEl);
-
-    // get main content element
-    mainContentEl = document.querySelector(".main-content");
-    // add quiz section to main content element
-    mainContentEl.appendChild(quizSectionEl);
 }
 
-createQuizEl();
+var addQuestionInfo = function(questionTitleEl, answerBtn1, answerBtn2, answerBtn3, answerBtn4, i) {
+    // All quiz question/answer object variables saved in an array
+    var allQuestions = [quizQuestion1, quizQuestion2, quizQuestion3, quizQuestion4, quizQuestion5]
+
+    questionTitleEl.innerHTML = allQuestions[i].questionText;
+    answerBtn1.innerHTML = "<li>" + allQuestions[i].answer1 + "</li>";
+    answerBtn2.innerHTML = "<li>" + allQuestions[i].answer2 + "</li>";
+    answerBtn3.innerHTML = "<li>" + allQuestions[i].answer3 + "</li>";
+    answerBtn4.innerHTML = "<li>" + allQuestions[i].answer4 + "</li>";
+    }
+
+
+mainContentEl.addEventListener("click", createQuizEl);
+
+
 
 
 
